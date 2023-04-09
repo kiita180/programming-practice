487bb6f3224d6270dbc658a1f6715d0e455ce3ad
limdonggyu <ehdrb180@gmail.com>
2023-04-09 15:57:34 +0900

    first commit

---
+++ third week practice 1.cpp
@@ -0,0 +1,16 @@
+﻿#include<stdio.h>
+
+int main(void)
+{
+	float radius;//원의 반지름
+	float area;//면적
+
+	printf("반지름을 입력하시오:");
+	scanf_s("%lf", &radius);
+
+	area = 3.14 * radius * radius;
+
+	printf("원의 면적: %lf\n", area);
+	return 0;
+
+}
